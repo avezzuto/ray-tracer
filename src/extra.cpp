620678199dbad4d2d91618ad7aeadd3c6ce64de0
Tudor Coman <t.a.coman@student.tudelft.nl>
2023-11-03 00:37:09 +0100

    Update file extra.cpp

--- src/extra.cpp
+++ src/extra.cpp
@@ -95,7 +95,69 @@ glm::vec3 sampleEnvironmentMap(RenderState& state, Ray ray)
 // This method is unit-tested, so do not change the function signature.
 size_t splitPrimitivesBySAHBin(const AxisAlignedBox& aabb, uint32_t axis, std::span<BVH::Primitive> primitives)
 {
-    using Primitive = BVH::Primitive;
+   const float_t startF = 0.3, increment = 0.05;
+std::vector<double> axisReducedCenters;
+size_t psize = primitives.size();
+//Calculate centers and reduce to the axis coordinate only
+for (int i = 0; i < psize; i++) {
+    glm::vec3 center = computePrimitiveCentroid(primitives[i]);
+    axisReducedCenters.push_back(center[axis]);
+}
+std::sort(axisReducedCenters.begin(), axisReducedCenters.end());
 
-    return 0; // This is clearly not the solution
+//geometric computations
+float_t SA;
+switch (axis) {
+case 0:
+    SA = (aabb.upper[1] - aabb.lower[1]) * (aabb.upper[2] - aabb.lower[2]);
+    break;
+case 1:
+    SA = (aabb.upper[0] - aabb.lower[0]) * (aabb.upper[2] - aabb.lower[2]);
+    break;
+default:
+    SA = (aabb.upper[0] - aabb.lower[0]) * (aabb.upper[1] - aabb.lower[1]);
+    break;
+}
+float_t axisSpan = aabb.upper[axis] - aabb.lower[axis];
+float_t totalVolume = axisSpan * SA;
+int bo = 0;
+size_t buckets[9] = {0,0,0,0,0,0,0,0,0};
+for (int i = 0; i < psize; i++) {
+    printf("i%d bo%d %lld ", i, bo, buckets[bo]);
+    if (axisReducedCenters[i] > aabb.upper[axis])
+        printf("over '\n'");
+    axisReducedCenters[i] -= aabb.lower[axis];
+    while (axisReducedCenters[i] >  axisSpan * (startF + increment * bo)) 
+        if (bo < 8)
+            bo++;
+    if (bo == 8) {
+        buckets[bo] += psize - i - 1;
+        break;
+    }
+    buckets[bo]++;
+}
+size_t numPrimitivesA = buckets[0];
+size_t numPrimitivesB = psize - numPrimitivesA;
+float_t volumeA = startF * totalVolume;
+float_t volumeB = totalVolume - volumeA;
+float_t splitMax = INFINITY;
+for (int i = 0; i < 9; i++) {
+
+    // Update the number of primitives in bins A and B
+    numPrimitivesA += buckets[i];
+    numPrimitivesB -= buckets[i];
+
+    // Update the surface area of AABBs A and B
+    volumeA += increment * totalVolume;
+    volumeB -= increment * totalVolume;
+
+    // Calculate the cost for bins A and B
+    float costA = numPrimitivesA * volumeA / totalVolume;
+    float costB = numPrimitivesB * volumeB / totalVolume;
+
+    // Calculate the total cost for the pair
+    if (costA + costB < splitMax)
+        splitMax = costA + costB;
+}
+return splitMax;
 }
